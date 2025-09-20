7596fc15dbfdb92025ccb1f34023328d6253bfce
pogifons <fonsandrei@gmail.com>
2025-09-21 00:55:41 +0800

    feat(modal): implement modal open/close functionality with DOM manipulation and helper functions

--- 05-modal/05-modal/starter/script.js
+++ 05-modal/05-modal/starter/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+// Selections
+const modalEl = document.querySelector('.modal');
+const overlayEl = document.querySelector('.overlay');
+const btnCloseModalEl = document.querySelector('.close-modal');
+const btnsOpenModalEl = document.querySelectorAll('.show-modal');
+
+// Helper functions
+const openModal = function () {
+  modalEl.classList.remove('hidden');
+  overlayEl.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modalEl.classList.add('hidden');
+  overlayEl.classList.add('hidden');
+};
+
+// Event listeners
+btnsOpenModalEl.forEach(btn => btn.addEventListener('click', openModal));
+btnCloseModalEl.addEventListener('click', closeModal);
+overlayEl.addEventListener('click', closeModal);
+
